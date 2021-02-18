6b37eb0dc6d6b573a573897d4681fb0d5d89c7a6
aneesurrehman001 <55150455+aneesurrehman001@users.noreply.github.com>
2021-02-18 06:14:25 -0500

    Added Interfaces.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,18 +13,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext Context;
-        public ProductsController(StoreContext context)
+
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo)
         {
-            Context = context;
+            _repo = repo;
+
+
         }
 
         [HttpGet]
-        
+
 
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await Context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
             return Ok(products);
 
         }
@@ -31,9 +36,9 @@ namespace API.Controllers
 
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await Context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
-            
+
 
 
     }
