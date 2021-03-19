86f64824bb70c228a703844984f474bdb14ee07a
aneesurrehman001 <55150455+aneesurrehman001@users.noreply.github.com>
2021-03-18 20:29:49 -0400

    end of section 4.

---
+++ API/Helpers/MappingProfiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using API.DTOs;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+            .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+            .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+
+            // The following code is making http://localhost:5001/ prefix of the image URL (Pictureurl)
+            // from => "images/products/sb-ts1.png"
+            // to => "http://localhost:5001/images/products/sb-ts1.png"
+
+            .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+        }
+    }
+}
